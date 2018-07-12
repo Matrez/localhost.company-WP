776045f273398c780d88b7cbc76cefdbd23792d8
Matej Strnisko <durifekete@gmail.com>
2018-07-12 12:28:08 +0200

    asd

--- script/showcase.js
+++ script/showcase.js
@@ -2,8 +2,9 @@ jQuery(function($){
     $(function(){
         $(".showcase-button").click(function(e){ // click event for load more
             e.preventDefault();
-            $(".showcase-project:hidden").slice(0, 4).show(); // select next 3 hidden divs and show them
-            if($(".showcase-project:hidden").length == 0){ // check if any hidden divs still exist
+            $(".showcase-project:hidden").slice(0, 4).addClass("animated fadeInDown");
+            $(".showcase-project:hidden").slice(0, 4).show();
+            if($(".showcase-project:hidden").length == 0){
                 $(".showcase-button").hide();
             }
         });
